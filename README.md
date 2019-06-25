efd6f9638b7b8a95592f3307e315f37b582ae142
leppott <Erik.Leppo@tetratech.com>
2019-06-25 13:31:13 -0400

    v0.1.0.9002
    Update model code and such.
    
    Model is at 100 MB, saved separately.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # FreshwaterExplorerModel
 
 This is where text will go that the user will see when they a directed to this web link.
+
+R code with random forest model is in the "model" folder.
--- model/RunningBackgroundECModel.R
+++ model/RunningBackgroundECModel.R
@@ -1,19 +1,44 @@
-### To apply natural background model to new data to predict natural background specific electrical conductivity (EC)
+### To apply natural background model to new data to predict natural background 
+# specific electrical conductivity (EC)
+
+# Packages
 library(randomForest)
 
-setwd("K:\\Data")#Set to working directory conatining model (Ref10av2.rf.rds) and prediction data
+#Set to working directory conatining model (Ref10av2.rf.rds) and prediction data
+#setwd("K:\\Data")
 
-RF_Pred_NatEC<-readRDS(file = "Ref10av2.rf.rds") #Read in model object
+# download files from GitHub (if necessary)
+# https://github.com/leppott/FreshwaterExplorerModel/tree/master/model
+# "ExamplePredictionData.csv" and "Ref10av2.rf.rds"
 
-RF_Pred_NatEC #Summary of model 
 
-Example_Predict_Data<-read.csv("ExamplePredictionData.csv", row.names="SampleID") #Read in prediction data
+#Read in model object
+RF_Pred_NatEC <- readRDS(file = "Ref10av2.rf.rds") 
 
-Example_Predict_Data
+#Summary of model
+RF_Pred_NatEC  
 
-all(c("mET12avg","Precip1","Precip3avg","Precip6avg","Tmax0","Tmax2","CaO_Mean","S_Mean","UCS_Mean","AtmCa_AVE","KffactWs","ClayWs","WtDepWs","PermWs","PctConif","PctMxFst","PctShrb","PctGrs","PctHbWet") %in% colnames(Example_Predict_Data)) #Check if all required predictors present and correctly labeled
+#Read in prediction data
+df_Example_Predict_Data <- read.csv("ExamplePredictionData.csv", row.names="SampleID") 
 
-Example_Predictions<-predict(RF_Pred_NatEC, newdata=Example_Predict_Data) # Runs new site data through model to predict backgroung EC
+# Show prediction data
+df_Example_Predict_Data
 
-write.csv(file="Example_Predictions.csv", Example_Predictions)#Saves predictions as csv
+#Check if all required predictors present and correctly labeled
+CheckALL <- all(c("mET12avg","Precip1","Precip3avg","Precip6avg","Tmax0","Tmax2"
+                  ,"CaO_Mean","S_Mean","UCS_Mean","AtmCa_AVE","KffactWs","ClayWs"
+                  ,"WtDepWs","PermWs","PctConif","PctMxFst","PctShrb","PctGrs"
+                  ,"PctHbWet") %in% colnames(df_Example_Predict_Data)) 
+
+# Run if pass QC check "CheckALL"
+if(CheckALL==TRUE){
+  # Runs new site data through model to predict backgroung EC
+  df_Example_Predict_Results <- predict(RF_Pred_NatEC, newdata=df_Example_Predict_Data) 
+  
+  #Saves predictions as csv
+  write.csv(file="ExamplePredictionResults.csv", df_Example_Predict_Results)
+}
+
+# Show prediction results
+df_Example_Predict_Results
 
